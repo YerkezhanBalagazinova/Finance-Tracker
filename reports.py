9c1f36d0d4301a6f1b5dfb3c045e018202a9c4ba
Anel Melis <anel@example.com>
2026-05-19 00:20:37 +0500

    2 func

--- reports.py
+++ reports.py
@@ -13,5 +13,18 @@ def month_sumex(transactions, year, month):
     
     return sumofexpenses
 
+def month_come(transactions, year, month):
+    sumofincomes=0
 
+    for t in transactions: 
+        if t["type"]=="income":
+            date_str=t["date"]
+            parts=date_str.split('-') 
+            trsn_year=int(parts[0])
+            trsn_month=int(parts[1])
+
+            if trsn_year==year and trsn_month==month:
+                sumofincomes+=t["amount"]
+    
+    return sumofincomes
 
