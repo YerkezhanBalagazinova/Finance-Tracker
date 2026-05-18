55b33c0b22e90f9aef7f63b0028bf69ff570da54
Anel Melis <anel@example.com>
2026-05-19 01:42:47 +0500

    main menu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import re
 from reports import month_exp, month_come, month_sum, exp_bycat,overspending
 
-def load_trns(filepath):
+def loadtrns(filepath):
     try:
         with open(filepath, "r", encoding='utf-8') as f:
             data=json.load(f)
@@ -13,3 +13,27 @@ def load_trns(filepath):
     except json.JSONDecodeError:
         print("Error, format JSON")
     
+
+def datacheck(datastr):
+    if re.match(r'^\d{4}-\d{2}-\d{2}$', datastr):
+       return True
+    else:
+       return False
+
+
+def main():
+    transactions = loadtrns("transaction.json")
+    if not transactions:
+        print("no data to work with ")
+
+    while True:
+      print("1)Monthly report")
+      print("2)Breakdown by category")
+      print("3)Overspending")
+      print("4)Exit")
+
+      choice = input("Select: ")
+      if not choice.isdigit():
+          print("select a number from 1 to 4")
+          continue
+      
